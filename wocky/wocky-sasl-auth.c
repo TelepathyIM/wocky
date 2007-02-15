5459d59a5a62c8a08394e9cfd122abc0c5f863e3
Sjoerd Simons <sjoerd@luon.net>
2007-02-15 16:06:10 +0000

    WockySaslAuth: Rename the error quak from salut_wocky_auth_failed to wocky_sasl_auth_error
    20070215160610-93b9a-5677d3a6fca4f248e779c043fdb6e8c7199fda77.gz

--- wocky/wocky-sasl-auth.c
+++ wocky/wocky-sasl-auth.c
@@ -74,7 +74,7 @@ wocky_sasl_auth_error_quark (void) {
   static GQuark quark = 0;
 
   if (!quark)
-    quark = g_quark_from_static_string ("salut_wocky_auth_failed");
+    quark = g_quark_from_static_string ("wocky_sasl_auth_error");
 
   return quark;
 }
