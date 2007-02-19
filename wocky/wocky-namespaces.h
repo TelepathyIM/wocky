4a364f0b3f688edf5370d375f2f44b5e077e857a
Sjoerd Simons <sjoerd@luon.net>
2007-02-19 22:52:17 +0000

    wocky-namespaces.h: Add TLS namespace
    20070219225217-93b9a-a939a12c3ca9cc6a087d08e9e3b045b0a2568f60.gz

--- wocky/wocky-namespaces.h
+++ wocky/wocky-namespaces.h
@@ -2,6 +2,9 @@
 #define WOCKY_XMPP_NS_STREAM \
   (const gchar *)"http://etherx.jabber.org/streams"
 
+#define WOCKY_XMPP_NS_TLS \
+  (const gchar *)"urn:ietf:params:xml:ns:xmpp-tls"
+
 #define WOCKY_XMPP_NS_SASL_AUTH \
   (const gchar *)"urn:ietf:params:xml:ns:xmpp-sasl"
 
