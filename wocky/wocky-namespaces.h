79388f1807076e7d7528102e11d11e0024ff9db1
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-06-15 11:55:34 +0000

    wocky-namespaces.h: add tubes NS
    20070615115534-7fe3f-bc808f4784fbc66fb160f25de84058a26800d4ea.gz

--- wocky/wocky-namespaces.h
+++ wocky/wocky-namespaces.h
@@ -23,4 +23,7 @@
 #define WOCKY_W3C_NS_XHTML \
   (const gchar *)"http://www.w3.org/1999/xhtml"
 
+#define WOCKY_TELEPATHY_NS_TUBES \
+  (const gchar *)"http://telepathy.freedesktop.org/xmpp/tubes"
+
 #endif /* #ifndef __WOCKY_NAMESPACES_H__ */
