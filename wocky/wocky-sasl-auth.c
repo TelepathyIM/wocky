dccf0da6a9ff31acd71d47144989d1ec53f11716
Emanuele Aina <em@nerd.ocracy.org>
2007-07-01 13:55:50 +0000

    Fix two small memleak in WockySaslAuth
    20070701135550-f974e-01e921aff6b722104ea1262ac39980d2dbef55b5.gz

--- wocky/wocky-sasl-auth.c
+++ wocky/wocky-sasl-auth.c
@@ -720,6 +720,8 @@ wocky_sasl_auth_start_mechanism(WockySaslAuth *sasl,
 
       g_string_free(str, TRUE);
       g_free(cstr);
+      g_free(username);
+      g_free(password);
 
       priv->state = WOCKY_SASL_AUTH_STATE_PLAIN_STARTED;
       break;
