1d2ca3485c2facd14de350faa5fde2c43ee580bf
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2007-07-02 07:57:48 +0000

    Correctly restart the xmpp connection after the auth was successfull
    20070702075748-b58c9-213277df2d2cb5e38c824542ea6356c1b80f0c0a.gz

--- tests/wocky-test-sasl-auth.c
+++ tests/wocky-test-sasl-auth.c
@@ -58,6 +58,7 @@ void
 auth_success(WockySaslAuth *sasl, gpointer user_data) {
   authenticated = TRUE;
   /* Reopen the connection */
+  wocky_xmpp_connection_restart(conn);
   wocky_xmpp_connection_open(conn, servername, NULL, "1.0");
 }
 
