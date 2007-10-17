f0f1ac83fd9a864adb6d3428332b5627ba476d98
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-10-17 14:51:15 +0000

    add jingle errors NS
    20071017145115-7fe3f-9045aaae3cf2efbd008ef6dd4b968e73cf609ad9.gz

--- wocky/wocky-namespaces.h
+++ wocky/wocky-namespaces.h
@@ -44,4 +44,7 @@
 #define WOCKY_XMPP_NS_OOB \
   (const gchar *)"jabber:iq:oob"
 
+#define WOCKY_XMPP_NS_JINGLE_ERRORS \
+  (const gchar *)"http://jabber.org/protocol/jingle#errors"
+
 #endif /* #ifndef __WOCKY_NAMESPACES_H__ */
