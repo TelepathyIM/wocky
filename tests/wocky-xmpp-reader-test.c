67b87e93fb445aed88049d05480711627f8b1759
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2009-04-03 17:00:36 +0100

    Add a pure reader test

---
+++ tests/wocky-xmpp-reader-test.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <glib.h>
+
+#include <wocky/wocky-xmpp-reader.h>
+#include <wocky/wocky-utils.h>
+
+#define HEADER \
+"<?xml version='1.0' encoding='UTF-8'?>                                    " \
+"<stream:stream xmlns='jabber:client'                                      " \
+"  xmlns:stream='http://etherx.jabber.org/streams'>                        "
+
+#define BROKEN_HEADER \
+"<?xml version='1.0' encoding='UTF-8'?>                                    " \
+"<stream:streamsss xmlns='jabber:client'                                   " \
+"  xmlns:stream='http://etherx.jabber.org/streams'>                        "
+
+#define BROKEN_MESSAGE \
+"  <message to='juliet@example.com' from='romeo@example.net' xml:lang='en' " \
+"   id=\"0\">                                                              " \
+"    <body>Art thou not Romeo, and a Montague?</ody>                       " \
+"  </essage>                                                               "
+
+static void
+test_stream_open_error (void)
+{
+  WockyXmppReader *reader;
+  GError *error;
+
+  reader = wocky_xmpp_reader_new ();
+
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_INITIAL);
+
+  wocky_xmpp_reader_push (reader,
+    (guint8 *) BROKEN_HEADER, strlen (BROKEN_HEADER));
+
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_ERROR);
+
+  error = wocky_xmpp_reader_get_error (reader);
+
+  g_assert (error != NULL);
+  g_assert (g_error_matches (error, WOCKY_XMPP_READER_ERROR,
+    WOCKY_XMPP_READER_ERROR_INVALID_STREAM_START));
+
+  g_object_unref (reader);
+}
+
+static void
+test_parse_error (void)
+{
+  WockyXmppReader *reader;
+  GError *error;
+
+  reader = wocky_xmpp_reader_new ();
+
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_INITIAL);
+
+  wocky_xmpp_reader_push (reader,
+    (guint8 *) HEADER, strlen (HEADER));
+
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_OPENED);
+
+  wocky_xmpp_reader_push (reader,
+    (guint8 *) BROKEN_MESSAGE, strlen (BROKEN_MESSAGE));
+
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_ERROR);
+  g_assert (wocky_xmpp_reader_pop_stanza (reader) == NULL);
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_ERROR);
+
+  error = wocky_xmpp_reader_get_error (reader);
+
+  g_assert (error != NULL);
+  g_assert (g_error_matches (error, WOCKY_XMPP_READER_ERROR,
+    WOCKY_XMPP_READER_ERROR_PARSE_ERROR));
+
+  g_object_unref (reader);
+}
+
+int
+main (int argc,
+    char **argv)
+{
+  g_thread_init (NULL);
+
+  g_test_init (&argc, &argv, NULL);
+  g_type_init ();
+
+  g_test_add_func ("/xmpp-reader/stream-open-error", test_stream_open_error);
+  g_test_add_func ("/xmpp-reader/parse-error", test_parse_error);
+
+  return g_test_run ();
+}
