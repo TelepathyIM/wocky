0eb0b3600d7c9a8377f4da24202af0616714daba
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2009-04-06 17:05:02 +0100

    Add a test for reader states when parsing chunks

--- tests/wocky-xmpp-reader-test.c
+++ tests/wocky-xmpp-reader-test.c
@@ -23,6 +23,15 @@
 "    <body>Art thou not Romeo, and a Montague?</ody>                       " \
 "  </essage>                                                               "
 
+#define MESSAGE_CHUNK0 \
+"  <message to='juliet@example.com' from='romeo@example.net' xml:lang='en' " \
+"   id=\"0\">                                                              " \
+"    <body>Art thou not Romeo,                                             "
+
+#define MESSAGE_CHUNK1 \
+"       and a Montague?</body>                                             " \
+"  </message>                                                              "
+
 static void
 test_stream_open_error (void)
 {
@@ -84,6 +93,39 @@ test_parse_error (void)
   g_object_unref (reader);
 }
 
+static void
+test_no_stream_hunks (void)
+{
+  WockyXmppReader *reader;
+  WockyXmppStanza *stanza;
+
+  reader = wocky_xmpp_reader_new_no_stream ();
+
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_OPENED);
+
+  wocky_xmpp_reader_push (reader,
+    (guint8 *) MESSAGE_CHUNK0, strlen (MESSAGE_CHUNK0));
+
+  g_assert (wocky_xmpp_reader_pop_stanza (reader) == NULL);
+
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_OPENED);
+
+  wocky_xmpp_reader_push (reader,
+    (guint8 *) MESSAGE_CHUNK1, strlen (MESSAGE_CHUNK1));
+
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_OPENED);
+
+  g_assert ((stanza = wocky_xmpp_reader_pop_stanza (reader)) != NULL);
+  g_assert (wocky_xmpp_reader_get_state (reader)
+    == WOCKY_XMPP_READER_STATE_CLOSED);
+
+  g_object_unref (stanza);
+  g_object_unref (reader);
+}
+
 int
 main (int argc,
     char **argv)
@@ -95,6 +137,7 @@ main (int argc,
 
   g_test_add_func ("/xmpp-reader/stream-open-error", test_stream_open_error);
   g_test_add_func ("/xmpp-reader/parse-error", test_parse_error);
+  g_test_add_func ("/xmpp-reader/no-stream-hunks", test_no_stream_hunks);
 
   return g_test_run ();
 }
