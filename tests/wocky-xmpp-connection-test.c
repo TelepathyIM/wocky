53abbaef35d5821b8a8284824c6681f041f62793
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2009-04-17 16:59:04 +0100

    Port the WockyXmppConnection tests to the new API

--- tests/wocky-xmpp-connection-test.c
+++ tests/wocky-xmpp-connection-test.c
@@ -30,29 +30,50 @@ test_instantiation (void)
 
   g_object_unref (connection);
   g_object_unref (stream);
-
-  connection = wocky_xmpp_connection_new (NULL);
-
-  g_assert (connection != NULL);
-
-  g_object_unref (connection);
 }
 
-static void
-stanza_received_cb (WockyXmppConnection *connection,
-  WockyXmppStanza *stanza, gpointer user_data)
-{
-  gboolean *message_parsed = user_data;
+/* Simple message test */
+typedef struct {
+  GMainLoop *loop;
+  gboolean parsed_stanza;
+} test_data_t;
 
-  *message_parsed = TRUE;
+static void
+stanza_received_cb (GObject *source, GAsyncResult *res, gpointer user_data)
+{
+  WockyXmppConnection *connection = WOCKY_XMPP_CONNECTION (source);
+  WockyXmppStanza *s;
+  test_data_t *data = (test_data_t *) user_data;
+  GError *error = NULL;
+
+  s = wocky_xmpp_connection_recv_stanza_finish (connection, res, &error);
+
+  if (!data->parsed_stanza)
+    {
+      g_assert (s != NULL);
+      data->parsed_stanza = TRUE;
+      wocky_xmpp_connection_recv_stanza_async (WOCKY_XMPP_CONNECTION (source),
+        NULL, stanza_received_cb, data);
+    }
+  else
+   {
+      g_assert (s == NULL);
+      g_main_loop_quit (data->loop);
+   }
 }
 
-static void
-parse_error_cb (WockyXmppConnection *connection, gpointer user_data)
-{
-  gboolean *parse_error_found = user_data;
 
-  *parse_error_found = TRUE;
+static void
+received_open_cb (GObject *source, GAsyncResult *res, gpointer user_data)
+{
+  WockyXmppConnection *conn = WOCKY_XMPP_CONNECTION (source);
+
+  if (!wocky_xmpp_connection_recv_open_finish (conn, res,
+      NULL, NULL, NULL, NULL, NULL))
+    g_assert_not_reached ();
+
+  wocky_xmpp_connection_recv_stanza_async (WOCKY_XMPP_CONNECTION (source),
+    NULL, stanza_received_cb, user_data);
 }
 
 #define CHUNK_SIZE 13
@@ -67,16 +88,15 @@ timeout_cb (gpointer data)
 }
 
 static void
-test_simple_message (void)
+test_recv_simple_message (void)
 {
   WockyXmppConnection *connection;
   WockyTestStream *stream;
   gsize len;
   gsize offset = 0;
-  gboolean parse_error_found = FALSE;
-  gboolean message_parsed = FALSE;
   gchar message[] = SIMPLE_MESSAGE;
   GMainLoop *loop = NULL;
+  test_data_t data = { NULL, FALSE };
 
   loop = g_main_loop_new (NULL, FALSE);
 
@@ -85,28 +105,25 @@ test_simple_message (void)
   stream = g_object_new (WOCKY_TYPE_TEST_STREAM, NULL);
   connection = wocky_xmpp_connection_new (stream->stream0);
 
-  g_signal_connect (connection, "received-stanza",
-      G_CALLBACK(stanza_received_cb), &message_parsed);
+  g_timeout_add (1000, timeout_cb, NULL);
 
-  g_signal_connect (connection, "parse-error",
-      G_CALLBACK(parse_error_cb), &parse_error_found);
+  data.loop = loop;
+  wocky_xmpp_connection_recv_open_async (connection,
+      NULL, received_open_cb, &data);
 
-  while (!parse_error_found && offset < len)
+  while (offset < len)
     {
       guint l = MIN (len - offset, CHUNK_SIZE);
+
+      while (g_main_context_iteration (NULL, FALSE))
+        ;
       g_output_stream_write_all (stream->stream1_output,
         message + offset, l, NULL, NULL, NULL);
       offset += l;
+
     }
 
-  g_timeout_add (1000, timeout_cb, NULL);
-
-  while (g_main_context_iteration (NULL, TRUE) && !message_parsed)
-    ;
-
-  g_assert (!parse_error_found);
-  g_assert (message_parsed);
-
+  g_main_loop_run (loop);
   g_main_loop_unref (loop);
 
   g_object_unref (stream);
@@ -122,8 +139,8 @@ main (int argc, char **argv)
   g_type_init ();
 
   g_test_add_func ("/xmpp-connection/initiation", test_instantiation);
-  g_test_add_func ("/xmpp-connection/simpe-message",
-    test_simple_message);
+  g_test_add_func ("/xmpp-connection/recv-simple-message",
+    test_recv_simple_message);
 
   return g_test_run ();
 }
