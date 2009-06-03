57ba9e784df0a446a22899fb189b696b0fceea5f
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-06-03 13:49:27 +0100

    wocky-xmpp-scheduler-test: use test helper

--- tests/wocky-xmpp-scheduler-test.c
+++ tests/wocky-xmpp-scheduler-test.c
@@ -6,72 +6,7 @@
 
 #include <wocky/wocky-xmpp-scheduler.h>
 #include "wocky-test-stream.h"
-
-typedef struct {
-  GMainLoop *loop;
-  GQueue *expected_stanzas;
-  WockyXmppConnection *in;
-  WockyXmppConnection *out;
-  WockyXmppScheduler *sched_in;
-  WockyXmppScheduler *sched_out;
-  WockyTestStream *stream;
-  guint outstanding;
-} test_data_t;
-
-static gboolean
-timeout_cb (gpointer data)
-{
-  g_test_message ("Timeout reached :(");
-  g_assert_not_reached ();
-
-  return FALSE;
-}
-
-static test_data_t *
-setup_test (void)
-{
-  test_data_t *data;
-
-  data = g_new0 (test_data_t, 1);
-  data->loop = g_main_loop_new (NULL, FALSE);
-
-  data->stream = g_object_new (WOCKY_TYPE_TEST_STREAM, NULL);
-  data->in = wocky_xmpp_connection_new (data->stream->stream0);
-  data->out = wocky_xmpp_connection_new (data->stream->stream1);
-
-  data->sched_in = wocky_xmpp_scheduler_new (data->in);
-  data->sched_out = wocky_xmpp_scheduler_new (data->out);
-
-  data->expected_stanzas = g_queue_new ();
-
-  g_timeout_add (1000, timeout_cb, NULL);
-
-  return data;
-}
-
-static void
-teardown_test (test_data_t *data)
-{
-  g_main_loop_unref (data->loop);
-  g_object_unref (data->stream);
-  g_object_unref (data->in);
-  g_object_unref (data->out);
-  g_object_unref (data->sched_in);
-  g_object_unref (data->sched_out);
-
-  /* All the stanzas should have been received */
-  g_assert (g_queue_get_length (data->expected_stanzas) == 0);
-  g_queue_free (data->expected_stanzas);
-
-  g_free (data);
-}
-
-static void
-test_wait_pending (test_data_t *test)
-{
-  while (test->outstanding > 0)
-    g_main_loop_run (test->loop);
-}
+#include "wocky-test-helper.h"
 
 static void
 test_instantiation (void)
