1fd895c4b547ee3c55b881491052306fa437ae95
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-06-16 12:54:31 +0100

    rename WockyXmppSchedulerStanzaCallbackFunc to WockyXmppSchedulerHandlerFunc

--- wocky/wocky-xmpp-scheduler.h
+++ wocky/wocky-xmpp-scheduler.h
@@ -92,7 +92,7 @@ void wocky_xmpp_scheduler_send (WockyXmppScheduler *scheduler,
 
 void wocky_xmpp_scheduler_start (WockyXmppScheduler *scheduler);
 
-typedef void (* WockyXmppSchedulerStanzaCallbackFunc) (
+typedef void (* WockyXmppSchedulerHandlerFunc) (
     WockyXmppScheduler *scheduler,
     WockyXmppStanza *stanza,
     gpointer user_data);
