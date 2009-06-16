f8114271ed5c26f1837b787c0fba3823ff45ef04
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-06-16 12:42:43 +0100

    wocky-xmpp-scheduler: remove unused priv->sending

--- wocky/wocky-xmpp-scheduler.c
+++ wocky/wocky-xmpp-scheduler.c
@@ -69,7 +69,6 @@ struct _WockyXmppSchedulerPrivate
 
   /* Queue of (sending_queue_elem *) */
   GQueue *sending_queue;
-  gboolean sending;
   GCancellable *receive_cancellable;
   /* List of (StanzaFilter *) */
   GSList *stanza_filters;
