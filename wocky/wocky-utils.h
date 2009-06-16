85ea3206514c56d3262bb74ec2e75b848cb65c81
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-06-16 16:47:16 +0100

    import wocky_decode_jid from Gabble

--- wocky/wocky-utils.h
+++ wocky/wocky-utils.h
@@ -27,6 +27,11 @@ G_BEGIN_DECLS
 gboolean wocky_strdiff (const gchar *left,
     const gchar *right);
 
+void wocky_decode_jid (const gchar *jid,
+    gchar **username_room,
+    gchar **server_service,
+    gchar **resource_nick);
+
 G_END_DECLS
 
 #endif /* #ifndef __WOCKY_UTIL_H__ */
