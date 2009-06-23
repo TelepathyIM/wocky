39f0cb5ed4afff87264210d3df7af85dccc278ea
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-06-23 14:48:21 +0200

    add a regression test for wocky_xmpp_node_equal

--- tests/wocky-xmpp-node-test.c
+++ tests/wocky-xmpp-node-test.c
@@ -7,6 +7,30 @@
 #include <wocky/wocky-xmpp-stanza.h>
 #include <wocky/wocky-utils.h>
 
+static void
+test_node_equal (void)
+{
+  WockyXmppStanza *a, *b;
+
+  /* Simple IQ node */
+  a = wocky_xmpp_stanza_build (WOCKY_STANZA_TYPE_IQ, WOCKY_STANZA_SUB_TYPE_SET,
+      "juliet@example.com", "romeo@example.org", WOCKY_STANZA_END);
+  g_assert (wocky_xmpp_node_equal (a->node, (a->node)));
+
+  /* Same as 'a' but with an ID attribute */
+  b = wocky_xmpp_stanza_build (WOCKY_STANZA_TYPE_IQ, WOCKY_STANZA_SUB_TYPE_SET,
+      "juliet@example.com", "romeo@example.org",
+      WOCKY_NODE_ATTRIBUTE, "id", "one",
+      WOCKY_STANZA_END);
+  g_assert (wocky_xmpp_node_equal (b->node, (b->node)));
+
+  g_assert (!wocky_xmpp_node_equal (a->node, (b->node)));
+  g_assert (!wocky_xmpp_node_equal (b->node, (a->node)));
+
+  g_object_unref (a);
+  g_object_unref (b);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -15,5 +39,6 @@ main (int argc, char **argv)
   g_test_init (&argc, &argv, NULL);
   g_type_init ();
 
+  g_test_add_func ("/xmpp-node/node-equale", test_node_equal);
   return g_test_run ();
 }
