36fa7099d59eafe8ac755907c20d3ac171437e9f
Jonny Lamb <jonny.lamb@collabora.co.uk>
2009-07-02 19:32:38 +0100

    Add test for fetch-roster.
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- tests/wocky-roster-test.c
+++ tests/wocky-roster-test.c
@@ -10,6 +10,7 @@
 #include <wocky/wocky-xmpp-connection.h>
 
 #include "wocky-test-stream.h"
+#include "wocky-test-helper.h"
 
 static void
 test_instantiation (void)
@@ -33,6 +34,60 @@ test_instantiation (void)
   g_object_unref (stream);
 }
 
+static gboolean
+fetch_roster_cb (WockyPorter *porter,
+    WockyXmppStanza *stanza,
+    gpointer user_data)
+{
+  test_data_t *test = (test_data_t *) user_data;
+  WockyStanzaType type;
+  WockyStanzaSubType sub_type;
+  WockyXmppNode *node;
+
+  /* Make sure stanza is as expected. */
+  wocky_xmpp_stanza_get_type_info (stanza, &type, &sub_type);
+
+  g_assert (type == WOCKY_STANZA_TYPE_IQ);
+  g_assert (sub_type == WOCKY_STANZA_SUB_TYPE_GET);
+
+  node = wocky_xmpp_node_get_child (stanza->node, "query");
+
+  g_assert (stanza->node != NULL);
+  g_assert (!wocky_strdiff (wocky_xmpp_node_get_ns (node),
+          "jabber:iq:roster"));
+
+  test->outstanding--;
+  g_main_loop_quit (test->loop);
+  return TRUE;
+}
+
+static void
+test_fetch_roster (void)
+{
+  WockyRoster *roster;
+  test_data_t *test = setup_test ();
+
+  test_open_both_connections (test);
+
+  wocky_porter_register_handler (test->sched_out,
+      WOCKY_STANZA_TYPE_IQ, WOCKY_STANZA_SUB_TYPE_GET, NULL,
+      WOCKY_PORTER_HANDLER_PRIORITY_MAX,
+      fetch_roster_cb, test, WOCKY_STANZA_END);
+
+  wocky_porter_start (test->sched_out);
+  wocky_porter_start (test->sched_in);
+
+  roster = wocky_roster_new (test->in, test->sched_in);
+
+  wocky_roster_fetch_roster (roster);
+  test->outstanding++;
+
+  test_wait_pending (test);
+
+  test_close_both_porters (test);
+  teardown_test (test);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -42,6 +97,7 @@ main (int argc, char **argv)
   g_type_init ();
 
   g_test_add_func ("/xmpp-roster/instantiation", test_instantiation);
+  g_test_add_func ("/xmpp-roster/fetch-roster", test_fetch_roster);
 
   return g_test_run ();
 }
