b0de5c42576571b75fc6c406e0cb9b54e833c7f4
Jonny Lamb <jonny.lamb@collabora.co.uk>
2009-07-02 16:29:33 +0100

    Initial WockyRoster implementation.
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

---
+++ wocky/wocky-roster.c
@@ -0,0 +1,248 @@
+/*
+ * wocky-roster.c - Source for WockyRoster
+ * Copyright (C) 2009 Collabora Ltd.
+ * @author Jonny Lamb <jonny.lamb@collabora.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * SECTION: wocky-roster
+ * @title: WockyRoster
+ * @short_description: TODO
+ *
+ * TODO
+ */
+
+#include <gio/gio.h>
+
+#include "wocky-roster.h"
+#include "wocky-namespaces.h"
+#include "wocky-xmpp-stanza.h"
+
+#define DEBUG_FLAG DEBUG_ROSTER
+#include "wocky-debug.h"
+
+G_DEFINE_TYPE (WockyRoster, wocky_roster, G_TYPE_OBJECT)
+
+/* properties */
+enum
+{
+  PROP_CONNECTION = 1,
+  PROP_PORTER,
+};
+
+/* signal enum */
+enum
+{
+  LAST_SIGNAL,
+};
+
+/*
+static guint signals[LAST_SIGNAL] = {0};
+*/
+
+/* private structure */
+typedef struct _WockyRosterPrivate WockyRosterPrivate;
+
+struct _WockyRosterPrivate
+{
+  WockyXmppConnection *conn;
+  WockyPorter *porter;
+  GHashTable *items;
+  guint iq_cb;
+
+  gboolean dispose_has_run;
+};
+
+#define WOCKY_ROSTER_GET_PRIVATE(o) \
+    (G_TYPE_INSTANCE_GET_PRIVATE ((o), WOCKY_TYPE_ROSTER, \
+    WockyRosterPrivate))
+
+static void
+wocky_roster_init (WockyRoster *obj)
+{
+  /*
+  WockyRoster *self = WOCKY_ROSTER (obj);
+  WockyRosterPrivate *priv = WOCKY_ROSTER_GET_PRIVATE (self);
+  */
+}
+
+static void
+wocky_roster_set_property (GObject *object,
+    guint property_id,
+    const GValue *value,
+    GParamSpec *pspec)
+{
+  WockyRosterPrivate *priv = WOCKY_ROSTER_GET_PRIVATE (object);
+
+  switch (property_id)
+    {
+    case PROP_CONNECTION:
+      priv->conn = g_value_get_object (value);
+      break;
+    case PROP_PORTER:
+      priv->porter = g_value_get_object (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+    }
+}
+
+static void
+wocky_roster_get_property (GObject *object,
+    guint property_id,
+    GValue *value,
+    GParamSpec *pspec)
+{
+  WockyRosterPrivate *priv = WOCKY_ROSTER_GET_PRIVATE (object);
+
+  switch (property_id)
+    {
+    case PROP_CONNECTION:
+      g_value_set_object (value, priv->conn);
+      break;
+    case PROP_PORTER:
+      g_value_set_object (value, priv->porter);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+      break;
+    }
+}
+
+static gboolean
+roster_iq_handler_cb (WockyPorter *porter,
+    WockyXmppStanza *stanza,
+    gpointer user_data)
+{
+  
+  return TRUE;
+}
+
+static void
+wocky_roster_constructed (GObject *object)
+{
+  WockyRoster *self = WOCKY_ROSTER (object);
+  WockyRosterPrivate *priv = WOCKY_ROSTER_GET_PRIVATE (self);
+
+  priv->items = g_hash_table_new (g_str_hash, g_str_equal);
+
+  priv->iq_cb = wocky_porter_register_handler (priv->porter,
+      WOCKY_STANZA_TYPE_IQ, WOCKY_STANZA_SUB_TYPE_RESULT, NULL,
+      WOCKY_PORTER_HANDLER_PRIORITY_NORMAL, roster_iq_handler_cb, self,
+      WOCKY_NODE, "query",
+        WOCKY_NODE_XMLNS, WOCKY_XMPP_NS_ROSTER,
+      WOCKY_NODE_END, WOCKY_STANZA_END);
+}
+
+static void
+wocky_roster_dispose (GObject *object)
+{
+  WockyRoster *self = WOCKY_ROSTER (object);
+  WockyRosterPrivate *priv = WOCKY_ROSTER_GET_PRIVATE (self);
+
+  if (priv->dispose_has_run)
+    return;
+
+  priv->dispose_has_run = TRUE;
+
+  if (priv->conn != NULL)
+    g_object_unref (priv->conn);
+
+  if (priv->porter != NULL)
+    g_object_unref (priv->porter);
+
+  if (G_OBJECT_CLASS (wocky_roster_parent_class)->dispose)
+    G_OBJECT_CLASS (wocky_roster_parent_class)->dispose (object);
+}
+
+static void
+wocky_roster_finalize (GObject *object)
+{
+  WockyRoster *self = WOCKY_ROSTER (object);
+  WockyRosterPrivate *priv = WOCKY_ROSTER_GET_PRIVATE (self);
+
+  g_hash_table_destroy (priv->items);
+
+  G_OBJECT_CLASS (wocky_roster_parent_class)->finalize (object);
+}
+
+static void
+wocky_roster_class_init (WockyRosterClass *wocky_roster_class)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (wocky_roster_class);
+
+  g_type_class_add_private (wocky_roster_class,
+      sizeof (WockyRosterPrivate));
+
+  object_class->constructed = wocky_roster_constructed;
+  object_class->set_property = wocky_roster_set_property;
+  object_class->get_property = wocky_roster_get_property;
+  object_class->dispose = wocky_roster_dispose;
+  object_class->finalize = wocky_roster_finalize;
+}
+
+WockyRoster *
+wocky_roster_new (WockyXmppConnection *conn,
+    WockyPorter *porter)
+{
+  g_return_val_if_fail (WOCKY_IS_XMPP_CONNECTION (conn), NULL);
+  g_return_val_if_fail (WOCKY_IS_PORTER (porter), NULL);
+
+  return g_object_new (WOCKY_TYPE_ROSTER,
+      "connection", conn,
+      "porter", porter,
+      NULL);
+}
+
+static void
+roster_recv_items_cb (GObject *source_object,
+    GAsyncResult *res,
+    gpointer user_data)
+{
+  GError *error = NULL;
+
+  if (!wocky_xmpp_connection_send_stanza_finish (
+          WOCKY_XMPP_CONNECTION (source_object), res, &error))
+    {
+      DEBUG ("Failed to receive roster items: %s",
+          error ? error->message : "no message");
+      g_clear_error (&error);
+      return;
+    }
+}
+
+void
+wocky_roster_recv_items (WockyRoster *self)
+{
+  WockyXmppStanza *stanza;
+  WockyRosterPrivate *priv;
+
+  g_return_if_fail (WOCKY_IS_ROSTER (self));
+
+  priv = WOCKY_ROSTER_GET_PRIVATE (self);
+
+  stanza = wocky_xmpp_stanza_build (WOCKY_STANZA_TYPE_IQ,
+      WOCKY_STANZA_SUB_TYPE_GET, "foo", NULL,
+        WOCKY_NODE, "query",
+          WOCKY_NODE_XMLNS, WOCKY_XMPP_NS_ROSTER,
+        WOCKY_NODE_END,
+      WOCKY_STANZA_END);
+
+  wocky_porter_send_iq_async (priv->porter,
+      stanza, NULL, roster_recv_items_cb, self);
+}
---
+++ wocky/wocky-roster.h
@@ -0,0 +1,69 @@
+/*
+ * wocky-roster.h - Header for WockyRoster
+ * Copyright (C) 2009 Collabora Ltd.
+ * @author Jonny Lamb <jonny.lamb@collabora.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __WOCKY_ROSTER_H__
+#define __WOCKY_ROSTER_H__
+
+#include <glib-object.h>
+
+#include "wocky-xmpp-connection.h"
+#include "wocky-porter.h"
+
+G_BEGIN_DECLS
+
+typedef struct _WockyRoster WockyRoster;
+typedef struct _WockyRosterClass WockyRosterClass;
+
+GQuark wocky_roster_error_quark (void);
+
+struct _WockyRosterClass {
+  GObjectClass parent_class;
+};
+
+struct _WockyRoster {
+  GObject parent;
+};
+
+GType wocky_roster_get_type (void);
+
+#define WOCKY_TYPE_ROSTER \
+  (wocky_roster_get_type ())
+#define WOCKY_ROSTER(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), WOCKY_TYPE_ROSTER, \
+   WockyRoster))
+#define WOCKY_ROSTER_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass), WOCKY_TYPE_ROSTER, \
+   WockyRosterClass))
+#define WOCKY_IS_ROSTER(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), WOCKY_TYPE_ROSTER))
+#define WOCKY_IS_ROSTER_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass), WOCKY_TYPE_ROSTER))
+#define WOCKY_ROSTER_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), WOCKY_TYPE_ROSTER, \
+   WockyRosterClass))
+
+WockyRoster * wocky_roster_new (WockyXmppConnection *conn,
+    WockyPorter *porter);
+
+void wocky_roster_recv_items (WockyRoster *roster);
+
+G_END_DECLS
+
+#endif /* #ifndef __WOCKY_ROSTER_H__*/
