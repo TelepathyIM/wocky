bc8c40454043b536867fb763148897f309ea8076
Jonny Lamb <jonny.lamb@collabora.co.uk>
2009-07-14 12:07:17 +0100

    Test sending an reply to an IQ with no "to" attribute.
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- tests/wocky-xmpp-stanza-test.c
+++ tests/wocky-xmpp-stanza-test.c
@@ -63,6 +63,30 @@ test_build_iq_result (void)
   g_object_unref (reply);
   g_object_unref (expected);
   g_object_unref (iq);
+
+  /* Send a reply to an IQ with no "to" attribute. */
+  iq = wocky_xmpp_stanza_build (WOCKY_STANZA_TYPE_IQ,
+    WOCKY_STANZA_SUB_TYPE_GET, "juliet@example.com", NULL,
+    WOCKY_NODE_ATTRIBUTE, "id", "one",
+      WOCKY_NODE, "query",
+        WOCKY_NODE_XMLNS, "http://jabber.org/protocol/disco#items",
+      WOCKY_NODE_END,
+    WOCKY_STANZA_END);
+
+  /* Send a simple ACK */
+  expected = wocky_xmpp_stanza_build (WOCKY_STANZA_TYPE_IQ,
+    WOCKY_STANZA_SUB_TYPE_RESULT, NULL, "juliet@example.com",
+    WOCKY_NODE_ATTRIBUTE, "id", "one",
+    WOCKY_STANZA_END);
+
+  reply = wocky_xmpp_stanza_build_iq_result (iq, WOCKY_STANZA_END);
+
+  g_assert (reply != NULL);
+  g_assert (wocky_xmpp_node_equal (reply->node, expected->node));
+
+  g_object_unref (reply);
+  g_object_unref (expected);
+  g_object_unref (iq);
 }
 
 static void
