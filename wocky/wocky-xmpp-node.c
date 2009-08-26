c524818f6f93b1b0fb5ec85553e4e8c6c4c8a7e4
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-08-26 14:51:43 +0100

    fix wocky_xmpp_node_unpack_error doc

--- wocky/wocky-xmpp-node.c
+++ wocky/wocky-xmpp-node.c
@@ -418,15 +418,18 @@ wocky_xmpp_node_get_first_child (WockyXmppNode *node)
 }
 
 /**
- * wocky_xmpp_node_unpack_error - given an XMPP Stanza error #WockyXmppNode
- * (see RFC 3920) this function extracts useful error info:
+ * wocky_xmpp_node_unpack_error:
  *
+ * @node: a #WockyXmppNode
  * @type: gchar ** into which to write the XMPP Stanza error type
  * @text: #WockyXmppNode ** to hold the node containing the error description
  * @orig: #WockyXmppNode ** to hold the original XMPP Stanza that triggered
  *        the error: XMPP does not require this to be provided in the error
  * @extra: #WockyXmppNode ** to hold any extra domain-specific XML tags
  *         for the error received.
+ *
+ * Given an XMPP Stanza error #WockyXmppNode see RFC 3920) this function
+ * extracts useful error info.
  *
  * The above parameters are all optional, pass NULL to ignore them.
  *
