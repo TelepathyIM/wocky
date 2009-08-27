0fed93aa2df5d8c514b2108023351892b7ee875d
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-08-27 11:00:44 +0100

    add wocky-resource-contact-test.c

---
+++ tests/wocky-resource-contact-test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <glib.h>
+
+#include <wocky/wocky-resource-contact.h>
+#include <wocky/wocky-utils.h>
+
+#include "wocky-test-helper.h"
+
+static void
+test_instantiation (void)
+{
+  WockyBareContact *bare_contact;
+  WockyResourceContact *resource_contact;
+
+  bare_contact = g_object_new (WOCKY_TYPE_BARE_CONTACT,
+      "jid", "juliet@example.net",
+      NULL);
+
+  resource_contact = g_object_new (WOCKY_TYPE_RESOURCE_CONTACT,
+      "resource", "Balcony",
+      "bare-contact", bare_contact,
+      NULL);
+
+  g_assert (WOCKY_IS_RESOURCE_CONTACT (resource_contact));
+  /* WockyResourceContact is a sub-class of WockyContact */
+  g_assert (WOCKY_IS_CONTACT (resource_contact));
+  g_object_unref (resource_contact);
+
+  resource_contact = wocky_resource_contact_new (bare_contact, "Balcony");
+  g_assert (WOCKY_IS_RESOURCE_CONTACT (resource_contact));
+  g_assert (WOCKY_IS_CONTACT (resource_contact));
+  g_object_unref (resource_contact);
+
+  g_object_unref (bare_contact);
+}
+
+int
+main (int argc, char **argv)
+{
+  int result;
+
+  test_init (argc, argv);
+
+  g_test_add_func ("/resource-contact/instantiation", test_instantiation);
+
+  result = g_test_run ();
+  test_deinit ();
+  return result;
+}
