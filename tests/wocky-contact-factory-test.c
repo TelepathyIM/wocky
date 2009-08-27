27a673970917f4fd9814be8a9bfb6a5cfb26cff6
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-08-27 13:11:53 +0100

    test wocky_contact_factory_lookup_bare_contact

--- tests/wocky-contact-factory-test.c
+++ tests/wocky-contact-factory-test.c
@@ -39,6 +39,32 @@ test_ensure_bare_contact(void)
   g_object_unref (b);
 }
 
+static void
+test_lookup_bare_contact (void)
+{
+  WockyContactFactory *factory;
+  WockyBareContact *contact;
+
+  factory = wocky_contact_factory_new ();
+
+  g_assert (wocky_contact_factory_lookup_bare_contact (factory,
+        "juliet@example.org") == NULL);
+
+  contact = wocky_contact_factory_ensure_bare_contact (factory,
+      "juliet@example.org");
+
+  g_assert (wocky_contact_factory_lookup_bare_contact (factory,
+        "juliet@example.org") != NULL);
+
+  g_object_unref (contact);
+
+  /* contact has been disposed and so is not in the factory anymore */
+  g_assert (wocky_contact_factory_lookup_bare_contact (factory,
+        "juliet@example.org") == NULL);
+
+  g_object_unref (factory);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -49,6 +75,8 @@ main (int argc, char **argv)
   g_test_add_func ("/contact-factory/instantiation", test_instantiation);
   g_test_add_func ("/contact-factory/ensure-bare-contact",
       test_ensure_bare_contact);
+  g_test_add_func ("/contact-factory/lookup-bare-contact",
+      test_lookup_bare_contact);
 
   result = g_test_run ();
   test_deinit ();
