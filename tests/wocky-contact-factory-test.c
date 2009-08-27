bd32e4b5890bfb89348daccd563c908f9b60c5d1
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-08-27 16:17:50 +0100

    test wocky_contact_factory_ensure_resource_contact

--- tests/wocky-contact-factory-test.c
+++ tests/wocky-contact-factory-test.c
@@ -65,6 +65,38 @@ test_lookup_bare_contact (void)
   g_object_unref (factory);
 }
 
+static void
+test_ensure_resource_contact(void)
+{
+  WockyContactFactory *factory;
+  WockyBareContact *juliet;
+  WockyResourceContact *juliet_balcony, *juliet_pub, *a, *b;
+
+  factory = wocky_contact_factory_new ();
+  juliet = wocky_bare_contact_new ("juliet@example.org");
+  juliet_balcony = wocky_resource_contact_new (juliet, "Balcony");
+  juliet_pub = wocky_resource_contact_new (juliet, "Pub");
+
+  /* Bare contact isn't in the factory yet */
+  a = wocky_contact_factory_ensure_resource_contact (factory,
+      "juliet@example.org/Balcony");
+  g_assert (wocky_resource_contact_equal (a, juliet_balcony));
+  g_assert (wocky_contact_factory_lookup_bare_contact (factory,
+        "juliet@example.org") != NULL);
+
+  /* Bare contact is already in the factory */
+  b = wocky_contact_factory_ensure_resource_contact (factory,
+      "juliet@example.org/Pub");
+  g_assert (wocky_resource_contact_equal (b, juliet_pub));
+
+  g_object_unref (factory);
+  g_object_unref (juliet);
+  g_object_unref (juliet_balcony);
+  g_object_unref (juliet_pub);
+  g_object_unref (a);
+  g_object_unref (b);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -77,6 +109,8 @@ main (int argc, char **argv)
       test_ensure_bare_contact);
   g_test_add_func ("/contact-factory/lookup-bare-contact",
       test_lookup_bare_contact);
+  g_test_add_func ("/contact-factory/ensure-resource-contact",
+      test_ensure_resource_contact);
 
   result = g_test_run ();
   test_deinit ();
