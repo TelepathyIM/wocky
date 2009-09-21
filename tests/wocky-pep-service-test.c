bd958bdb099057e25dcfcdf0cf4997fdc2c9693b
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-09-21 17:40:12 +0100

    test wocky_pep_service_get_async

--- tests/wocky-pep-service-test.c
+++ tests/wocky-pep-service-test.c
@@ -105,6 +105,95 @@ test_changed_signal (void)
   g_assert (!event_received);
 }
 
+/* Test wocky_pep_service_get_async */
+static void
+test_send_query_cb (GObject *source_object,
+    GAsyncResult *res,
+    gpointer user_data)
+{
+  test_data_t *test = (test_data_t *) user_data;
+  WockyXmppStanza *reply;
+  WockyStanzaType type;
+  WockyStanzaSubType sub_type;
+
+  reply = wocky_pep_service_get_finish (WOCKY_PEP_SERVICE (source_object), res,
+      NULL);
+  g_assert (reply != NULL);
+
+  wocky_xmpp_stanza_get_type_info (reply, &type, &sub_type);
+  g_assert (type == WOCKY_STANZA_TYPE_IQ);
+  g_assert (sub_type == WOCKY_STANZA_SUB_TYPE_RESULT);
+
+  g_object_unref (reply);
+
+  test->outstanding--;
+  g_main_loop_quit (test->loop);
+}
+
+static gboolean
+test_send_query_stanza_received_cb (WockyPorter *porter,
+    WockyXmppStanza *stanza,
+    gpointer user_data)
+{
+  test_data_t *test = (test_data_t *) user_data;
+  WockyXmppStanza *reply;
+
+  reply = wocky_xmpp_stanza_build_iq_result (stanza,
+      WOCKY_NODE, "pubsub",
+        WOCKY_NODE_XMLNS, WOCKY_XMPP_NS_PUBSUB,
+        WOCKY_NODE, "items",
+          WOCKY_NODE_ATTRIBUTE, "node", "node1",
+          WOCKY_NODE, "item",
+            WOCKY_NODE_ATTRIBUTE, "id", "1",
+            WOCKY_NODE, "payload", WOCKY_NODE_END,
+          WOCKY_NODE_END,
+        WOCKY_NODE_END,
+      WOCKY_NODE_END,
+      WOCKY_STANZA_END);
+
+  wocky_porter_send (porter, reply);
+  g_object_unref (reply);
+
+  test->outstanding--;
+  g_main_loop_quit (test->loop);
+  return TRUE;
+}
+
+static void
+test_get (void)
+{
+  test_data_t *test = setup_test ();
+  WockyPepService *pep;
+  WockyContactFactory *contact_factory;
+  WockyBareContact *contact;
+
+  pep = wocky_pep_service_new (TEST_NODE1, FALSE);
+  test_open_both_connections (test);
+
+  wocky_pep_service_start (pep, test->session_in);
+  wocky_porter_start (test->sched_out);
+  wocky_porter_start (test->sched_in);
+
+  wocky_porter_register_handler (test->sched_out,
+      WOCKY_STANZA_TYPE_IQ, WOCKY_STANZA_SUB_TYPE_GET, NULL,
+      WOCKY_PORTER_HANDLER_PRIORITY_MAX,
+      test_send_query_stanza_received_cb, test, WOCKY_STANZA_END);
+
+  contact_factory = wocky_session_get_contact_factory (test->session_in);
+  contact = wocky_contact_factory_ensure_bare_contact (contact_factory,
+      "juliet@example.org");
+
+  wocky_pep_service_get_async (pep, contact, NULL, test_send_query_cb, test);
+  g_object_unref (contact);
+
+  test->outstanding += 2;
+  test_wait_pending (test);
+
+  test_close_both_porters (test);
+  g_object_unref (pep);
+  teardown_test (test);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -114,6 +203,7 @@ main (int argc, char **argv)
 
   g_test_add_func ("/pep-service/instantiation", test_instantiation);
   g_test_add_func ("/pep-service/changed-signal", test_changed_signal);
+  g_test_add_func ("/pep-service/get", test_get);
 
   result = g_test_run ();
   test_deinit ();
