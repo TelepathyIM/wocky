a7f5b70e57d420c0c1e5b8838d224ce0a35a95a2
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-09-22 13:32:12 +0100

    test wocky_pubsub_service_ensure_node

--- tests/wocky-pubsub-service-test.c
+++ tests/wocky-pubsub-service-test.c
@@ -13,10 +13,9 @@
 
 
 /* Test to instantiate a WockyPubsubService object */
-static void
-test_instantiation (void)
+static WockySession *
+create_session (void)
 {
-  WockyPubsubService *pubsub;
   WockyXmppConnection *connection;
   WockyTestStream *stream;
   WockySession *session;
@@ -25,13 +24,53 @@ test_instantiation (void)
   connection = wocky_xmpp_connection_new (stream->stream0);
   session = wocky_session_new (connection);
 
+  g_object_unref (connection);
+  g_object_unref (stream);
+  return session;
+}
+
+static void
+test_instantiation (void)
+{
+  WockyPubsubService *pubsub;
+  WockySession *session;
+
+  session = create_session ();
+
   pubsub = wocky_pubsub_service_new (session, "pubsub.localhost");
   g_assert (pubsub != NULL);
 
   g_object_unref (pubsub);
   g_object_unref (session);
-  g_object_unref (connection);
-  g_object_unref (stream);
+}
+
+/* Test wocky_pubsub_service_ensure_node */
+static void
+test_ensure_node (void)
+{
+  WockyPubsubService *pubsub;
+  WockySession *session;
+  WockyPubsubNode *node;
+
+  session = create_session ();
+
+  pubsub = wocky_pubsub_service_new (session, "pubsub.localhost");
+  node = wocky_pubsub_service_lookup_node (pubsub, "node1");
+  g_assert (node == NULL);
+
+  node = wocky_pubsub_service_ensure_node (pubsub, "node1");
+  g_assert (node != NULL);
+
+  node = wocky_pubsub_service_lookup_node (pubsub, "node1");
+  g_assert (node != NULL);
+
+  /* destroy the node */
+  g_object_unref (node);
+  node = wocky_pubsub_service_lookup_node (pubsub, "node1");
+  g_assert (node == NULL);
+
+  g_object_unref (pubsub);
+  g_object_unref (session);
 }
 
 int
@@ -42,6 +81,7 @@ main (int argc, char **argv)
   test_init (argc, argv);
 
   g_test_add_func ("/pubsub-service/instantiation", test_instantiation);
+  g_test_add_func ("/pubsub-service/ensure_node", test_ensure_node);
 
   result = g_test_run ();
   test_deinit ();
