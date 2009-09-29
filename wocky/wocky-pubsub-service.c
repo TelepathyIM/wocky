14bac97ce3675b1559219a931ab078d86f66e79d
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-09-29 16:47:03 +0100

    create_node_iq_cb: raise an error if server returned an error

--- wocky/wocky-pubsub-service.c
+++ wocky/wocky-pubsub-service.c
@@ -395,7 +395,12 @@ create_node_iq_cb (GObject *source,
       goto out;
     }
 
-  /* TODO: check success */
+  if (wocky_xmpp_stanza_extract_errors (reply, NULL, &error, NULL, NULL))
+    {
+      g_simple_async_result_set_from_error (result, error);
+      g_error_free (error);
+      goto out;
+    }
 
   name = g_simple_async_result_get_op_res_gpointer (result);
   node = wocky_pubsub_service_ensure_node (self, name);
