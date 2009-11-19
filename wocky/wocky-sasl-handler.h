6a51d50816f6d7b987437905351195b202b76b07
Dafydd Harries <dafydd.harries@collabora.co.uk>
2009-11-19 13:45:07 -0500

    add some documentation for the WockySaslHandler interface

--- wocky/wocky-sasl-handler.h
+++ wocky/wocky-sasl-handler.h
@@ -18,12 +18,38 @@ G_BEGIN_DECLS
 
 typedef struct _WockySaslHandler WockySaslHandler;
 
+/** WockySaslChallengeFunc:
+ * Called in two cases:
+ *
+ *  * When authentication begins, in case the mechanism allows a response to
+ *    an implicit challenge during SASL initiation (which, in XMPP,
+ *    corresponds to sending the <auth/> stanza to the server). In this case,
+ *    @stanza is NULL. The function may return NULL to indicate that it has no
+ *    initial response to send.
+ *  * During authentication, when a <challenge/> stanza is received. The
+ *    handler should return a response to the challenge.
+ *
+ * The handler is responsible for Base64-encoding responses if appropriate. In
+ * either case, the handler may return NULL and pass an error via @error to
+ * indicate that an error occurred.
+ **/
 typedef gchar * (*WockySaslChallengeFunc) (
     WockySaslHandler *handler, WockyXmppStanza *stanza, GError **error);
 
+/** WockySaslSuccessFunc:
+ * Called when a <success/> stanza is received during authentication. If no
+ * error is returned, then authentication is considered finished. (Typically,
+ * an error is only raised if the <success/> stanza was received earlier than
+ * expected.)
+ **/
 typedef void (*WockySaslSuccessFunc) (
     WockySaslHandler *handler, WockyXmppStanza *stanza, GError **error);
 
+/** WockySaslSuccessFunc:
+ * Called when a <failure/> stanza is received during authentication. The
+ * handler may provide a detailed error via @error. If no error is returned,
+ * authentication will fail with a general authentication error.
+ **/
 typedef void (*WockySaslFailureFunc) (
     WockySaslHandler *handler, WockyXmppStanza *stanza, GError **error);
 
@@ -56,6 +82,15 @@ wocky_sasl_handler_handle_failure (
 
 typedef struct _WockySaslHandlerIface WockySaslHandlerIface;
 
+/**
+ * WockySaslHandlerIface:
+ * @parent: The parent interface.
+ * @mechanism: The SASL mechanism which this handler responds to challenges
+ *    for.
+ * @challenge_func: Called when a <challenge/> stanza is received.
+ * @success_func: Called when a <success/> stanza is received.
+ * @ @failure_func: Called when a <failure/> stanza is received.
+ **/
 struct _WockySaslHandlerIface
 {
     GTypeInterface parent;
