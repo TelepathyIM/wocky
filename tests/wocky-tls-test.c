dec48abfe9390655299a8f717b3510c6c8632923
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2009-12-07 12:36:07 +0000

    Make the openssl test not be openssl specific
    Change the test to use the new modes in the WockyTestStream to force the way
    the openssl backend reads in the various records. This allows us to mostly use
    the test in case we're using the GnuTLS backend as well
    
    Also test for that the operation actually completes asynchronously.

--- tests/wocky-tls-test.c
+++ tests/wocky-tls-test.c
@@ -5,29 +5,28 @@
 #include "config.h"
 
 #include <glib.h>
-#if USING_OPENSSL
-#include <wocky/wocky-openssl.h>
-#else
-#error OpenSSL specific test binary: do not build for GnuTLS
-#endif
 
 #include <wocky/wocky-utils.h>
+#include <wocky/wocky-tls.h>
 #include "wocky-test-stream.h"
 #include "wocky-test-helper.h"
-#include "wocky-test-openssl-server.h"
 
 #define BUF_SIZE 8192
+
+#define TEST_SSL_DATA "badgerbadgerbadger"
+#define TEST_SSL_DATA_LEN sizeof (TEST_SSL_DATA)
 
 typedef struct {
   test_data_t *test;
   char cli_buf[BUF_SIZE];
   char srv_buf[BUF_SIZE];
   WockyTLSConnection *client;
-  TestTLSConnection *server;
+  WockyTLSConnection *server;
   GString *cli_data;
   GString *srv_data;
   guint read_op_count;
-  guint read_byte_count;
+  guint write_op_count;
+  gboolean in_read;
 } ssl_test_t;
 
 /* ************************************************************************ */
@@ -40,12 +39,14 @@ client_write_cb (GObject *source,
 {
   GOutputStream *output = G_OUTPUT_STREAM (source);
   ssl_test_t *ssl_test = data;
-  g_output_stream_write_finish (output, result, NULL);
+  GError *error = NULL;
+
+  g_assert (g_output_stream_write_finish (output, result, &error)
+    == TEST_SSL_DATA_LEN);
+  g_assert_no_error (error);
 
   ssl_test->test->outstanding--;
-
-  if (ssl_test->test->outstanding == 0)
-    g_main_loop_quit (ssl_test->test->loop);
+  g_main_loop_quit (ssl_test->test->loop);
 }
 
 static void
@@ -55,27 +56,50 @@ client_read_cb (GObject *source,
 {
   GInputStream *input = G_INPUT_STREAM (source);
   ssl_test_t *ssl_test = data;
-  gssize count = g_input_stream_read_finish (input, result, NULL);
+  GError *error = NULL;
+  gssize count = g_input_stream_read_finish (input, result, &error);
 
-  ssl_test->read_byte_count += count;
+  g_assert (!ssl_test->in_read);
+  g_assert_no_error (error);
+  g_assert_cmpint (count, ==, TEST_SSL_DATA_LEN);
 
-  ssl_test->test->outstanding--;
   ssl_test->read_op_count++;
 
   g_string_append_len (ssl_test->cli_data, ssl_test->cli_buf, count);
 
-  if (ssl_test->read_op_count >= 3)
+  switch (ssl_test->read_op_count)
     {
-      GIOStream *io = G_IO_STREAM (ssl_test->client);
-      GOutputStream *output = g_io_stream_get_output_stream (io);
-      g_output_stream_write_async (output,
-          TEST_SSL_DATA, TEST_SSL_DATA_LEN, G_PRIORITY_DEFAULT,
-          ssl_test->test->cancellable, client_write_cb, data);
-      return;
+      case 1:
+        /* we've read at least one ssl record at this point, combine the others
+         */
+        wocky_test_stream_set_mode (ssl_test->test->stream->stream0_input,
+         WOCK_TEST_STREAM_READ_COMBINE);
+       break;
+     case 2:
+#ifdef USING_OPENSSL
+        /* Our openssl backend should have read all ssl records by now and
+         * thus shouldn't request more data from the input stream. The GnuTLS
+         * backend requests more granularily what it needs so will still read
+         * from the input stream */
+        wocky_test_input_stream_set_read_error (
+         ssl_test->test->stream->stream0_input);
+#endif /* USING_OPENSSL */
+        break;
+     case 3:
+       {
+         GIOStream *io = G_IO_STREAM (ssl_test->client);
+         GOutputStream *output = g_io_stream_get_output_stream (io);
+         g_output_stream_write_async (output,
+           TEST_SSL_DATA, TEST_SSL_DATA_LEN, G_PRIORITY_DEFAULT,
+           ssl_test->test->cancellable, client_write_cb, data);
+         return;
+       }
     }
 
+  ssl_test->in_read = TRUE;
   g_input_stream_read_async (input, ssl_test->cli_buf, BUF_SIZE,
       G_PRIORITY_DEFAULT, ssl_test->test->cancellable, client_read_cb, data);
+  ssl_test->in_read = FALSE;
 }
 
 static void
@@ -90,11 +114,10 @@ client_handshake_cb (GObject *source,
   ssl_test->client = wocky_tls_session_handshake_finish (session, result, NULL);
   input = g_io_stream_get_input_stream (G_IO_STREAM (ssl_test->client));
 
-  ssl_test->test->outstanding--;
-  ssl_test->read_op_count = 0;
-
+  ssl_test->in_read = TRUE;
   g_input_stream_read_async (input, ssl_test->cli_buf, BUF_SIZE,
       G_PRIORITY_DEFAULT, ssl_test->test->cancellable, client_read_cb, data);
+  ssl_test->in_read = FALSE;
 }
 
 /* ************************************************************************ */
@@ -108,12 +131,10 @@ server_read_cb (GObject *source,
   ssl_test_t *ssl_test = data;
   gssize count = g_input_stream_read_finish (input, result, NULL);
 
-  ssl_test->test->outstanding--;
-
   g_string_append_len (ssl_test->srv_data, ssl_test->srv_buf, count);
 
-  if (ssl_test->test->outstanding == 0)
-    g_main_loop_quit (ssl_test->test->loop);
+  ssl_test->test->outstanding--;
+  g_main_loop_quit (ssl_test->test->loop);
 }
 
 
@@ -123,15 +144,27 @@ server_write_cb (GObject *source,
     gpointer data)
 {
   ssl_test_t *ssl_test = data;
+  gsize written;
   GOutputStream *output = G_OUTPUT_STREAM (source);
   GIOStream *io = G_IO_STREAM (ssl_test->server);
   GInputStream *input = g_io_stream_get_input_stream (io);
 
-  g_output_stream_write_finish (output, result, NULL);
-  ssl_test->test->outstanding--;
+  written = g_output_stream_write_finish (output, result, NULL);
 
-  g_input_stream_read_async (input, ssl_test->srv_buf, BUF_SIZE,
-      G_PRIORITY_DEFAULT, ssl_test->test->cancellable, server_read_cb, data);
+  g_assert (written == TEST_SSL_DATA_LEN);
+
+  ssl_test->write_op_count++;
+  if (ssl_test->write_op_count < 3)
+    {
+      g_output_stream_write_async (output, TEST_SSL_DATA, TEST_SSL_DATA_LEN,
+        G_PRIORITY_LOW, ssl_test->test->cancellable, server_write_cb, data);
+    }
+  else
+    {
+      wocky_test_stream_cork (ssl_test->test->stream->stream0_input, FALSE);
+      g_input_stream_read_async (input, ssl_test->srv_buf, BUF_SIZE,
+       G_PRIORITY_DEFAULT, ssl_test->test->cancellable, server_read_cb, data);
+    }
 }
 
 static void
@@ -140,15 +173,23 @@ server_handshake_cb (GObject *source,
     gpointer data)
 {
   GOutputStream *output;
-  TestTLSSession *session = TEST_TLS_SESSION (source);
+  WockyTLSSession *session = WOCKY_TLS_SESSION (source);
   ssl_test_t *ssl_test = data;
+  GError *error = NULL;
 
-  ssl_test->server = test_tls_session_handshake_finish (session, result, NULL);
+  ssl_test->server = wocky_tls_session_handshake_finish (session,
+    result, &error);
+
+  g_assert_no_error (error);
+  g_assert (ssl_test->server != NULL);
+
   output = g_io_stream_get_output_stream (G_IO_STREAM (ssl_test->server));
+
+  /* cork the client reading stream */
+  wocky_test_stream_cork (ssl_test->test->stream->stream0_input, TRUE);
+
   g_output_stream_write_async (output, TEST_SSL_DATA, TEST_SSL_DATA_LEN,
       G_PRIORITY_LOW, ssl_test->test->cancellable, server_write_cb, data);
-
-  ssl_test->test->outstanding--;
 }
 
 /* ************************************************************************ */
@@ -176,33 +217,33 @@ teardown_ssl_test (ssl_test_t *ssl_test)
 static void
 test_openssl_handshake_rw (void)
 {
-  ssl_test_t ssl_test;
+  ssl_test_t ssl_test = { NULL, } ;
   test_data_t *test = setup_test();
   WockyTLSSession *client = wocky_tls_session_new (test->stream->stream0);
-  TestTLSSession *server = test_tls_session_server_new (test->stream->stream1,
-      1024, TLS_SERVER_KEY_FILE, TLS_SERVER_CRT_FILE);
-  gsize expected = (TEST_SSL_RECORD_MARKER_LEN * 2) + TEST_SSL_DATA_LEN;
+  WockyTLSSession *server = wocky_tls_session_server_new (
+    test->stream->stream1, 1024, TLS_SERVER_KEY_FILE, TLS_SERVER_CRT_FILE);
+  gsize expected = TEST_SSL_DATA_LEN * 3;
   gchar *target =
-    TEST_SSL_RECORD_MARKER "\0" TEST_SSL_DATA "\0" TEST_SSL_RECORD_MARKER;
+    TEST_SSL_DATA "\0" TEST_SSL_DATA "\0" TEST_SSL_DATA;
 
   setup_ssl_test (&ssl_test, test);
 
   wocky_tls_session_handshake_async (client, G_PRIORITY_DEFAULT,
       test->cancellable, client_handshake_cb, &ssl_test);
-  test->outstanding += 5; /* handshake + 3 reads + 1 write */
+  test->outstanding += 1;
 
-  test_tls_session_handshake_async (server, G_PRIORITY_DEFAULT,
+  wocky_tls_session_handshake_async (server, G_PRIORITY_DEFAULT,
       test->cancellable, server_handshake_cb, &ssl_test);
-  test->outstanding += 3; /* handshake + 1 write + 1 read */
+  test->outstanding += 1;
 
   test_wait_pending (test);
 
   g_assert (test->outstanding == 0);
-  g_assert (ssl_test.read_op_count == 3);
   g_assert (ssl_test.cli_data->len == expected);
   g_assert (ssl_test.srv_data->len == TEST_SSL_DATA_LEN);
   g_assert (!memcmp (ssl_test.cli_data->str, target, expected));
-  g_assert (!memcmp (ssl_test.srv_data->str, TEST_SSL_DATA, TEST_SSL_DATA_LEN));
+  g_assert (!memcmp (ssl_test.srv_data->str,
+    TEST_SSL_DATA, TEST_SSL_DATA_LEN));
 
   teardown_test (test);
   teardown_ssl_test (&ssl_test);
