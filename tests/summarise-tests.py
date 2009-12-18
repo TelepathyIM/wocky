dbe94b97a0117b92a2146f77b5ade4aa28918b25
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2009-12-18 13:04:00 +0000

    Various improvements to the python summary
    Let the python summary cope with skipped and restarted tests and in case of
    failures print the cases that failed.

--- tests/summarise-tests.py
+++ tests/summarise-tests.py
@@ -7,39 +7,48 @@ import xml.dom.minidom as minidom
 def process(testbinary):
     cases = 0
     passes = 0
+    failures = []
 
     for e in testbinary.childNodes:
         if e.nodeType != e.ELEMENT_NODE or e.localName != 'testcase':
             continue
+        if e.hasAttribute('skipped'):
+            continue
+        path = e.getAttribute("path")
 
         cases = cases + 1
 
         status = e.getElementsByTagName('status')[0]
 
-        if status.getAttribute('result') == 'success':
-            passes = passes + 1
+        if status.getAttribute('result') != 'success':
+            failures += [ path ]
 
-    return (passes, cases)
+    return (cases, failures)
 
 doc = minidom.parse(sys.argv[1])
 
 okay = True
 
+tests = {}
 for e in doc.childNodes[0].childNodes:
     if e.nodeType != e.ELEMENT_NODE or e.localName != 'testbinary':
         continue
+    path = e.getAttribute("path")
 
-    name = e.getAttribute("path")
+    cases, failures  = process(e)
+    ocases, ofailures = tests.get (path, [ 0, []])
+    tests[path] = [ ocases + cases, ofailures + failures ]
 
-    passes, total = process(e)
-
-    if passes == total:
+for name, [cases, failures] in tests.iteritems():
+    if failures == []:
         result = 'PASS'
     else:
         result = 'FAIL'
         okay = False
 
-    print "%s: %s: %u/%u tests passed" % (result, name, passes, total)
+    print "%s: %s: %u/%u tests passed" % (result, name, cases - len (failures), cases)
+    for f in failures:
+        print "\tFailure: %s" % f
 
 if not okay:
     print "Disaster! Calamity!"
