129fcf4abc5223186b161883f099f2238b43cea7
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2010-01-04 13:01:27 +0100

    Only allow setting status directly when sending an unavailable presence

--- wocky/wocky-muc.c
+++ wocky/wocky-muc.c
@@ -812,9 +812,14 @@ wocky_muc_create_presence (WockyMuc *muc,
   WockyXmppNode *presence = stanza->node;
   WockyXmppNode *x = wocky_xmpp_node_add_child_ns (presence, "x", WOCKY_NS_MUC);
 
+
+  /* There should be seperate API to leave a room, but atm there isn't... so
+   * only allow the status to be set directly when making a presence to leave
+   * the muc */
+  g_assert (status == NULL || type == WOCKY_STANZA_SUB_TYPE_UNAVAILABLE);
+
   if (status != NULL)
     {
-      wocky_xmpp_node_add_child_with_content (presence, "show", "xa");
       wocky_xmpp_node_add_child_with_content (presence, "status", status);
     }
   else
