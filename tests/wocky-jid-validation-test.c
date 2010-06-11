cba01d1776b8e2193ddd5dc0d3f741807bda61d8
Will Thompson <will.thompson@collabora.co.uk>
2010-06-11 16:39:00 +0100

    Accept valid Unicode domains
    Previously we did not accept any non-ASCII domains, which meant we
    dropped perfectly valid messages from perfectly valid contacts. :o
    
    This patch accepts any non-ASCII characters in domains. This is still
    not right — for instance, the freshly-added test case demonstrates that
    spaces in domains are properly rejected, but non-breaking spaces are
    improperly accepted — but it's better than no validation at all (which
    was my previous attempt at fixing this) and avoids us dropping messages.

---
+++ tests/wocky-jid-validation-test.c
@@ -0,0 +1,56 @@
+#include <glib.h>
+
+#include <wocky/wocky-utils.h>
+
+#include "wocky-test-helper.h"
+
+static void
+valid (gconstpointer data)
+{
+  const gchar *jid = data;
+
+  g_assert (wocky_decode_jid (jid, NULL, NULL, NULL));
+}
+
+static void
+invalid (gconstpointer data)
+{
+  const gchar *jid = data;
+
+  g_assert (!wocky_decode_jid (jid, NULL, NULL, NULL));
+}
+
+int
+main (int argc, char **argv)
+{
+  int result;
+
+  test_init (argc, argv);
+
+  g_test_add_data_func ("/jid/valid/simple", "foo@bar.com/baz", valid);
+  g_test_add_data_func ("/jid/valid/no-resource", "foo@bar.com", valid);
+  g_test_add_data_func ("/jid/valid/no-node", "bar.com/baz", valid);
+  g_test_add_data_func ("/jid/valid/just-domain", "bar.com", valid);
+  g_test_add_data_func ("/jid/valid/ip", "foo@127.0.0.1", valid);
+  g_test_add_data_func ("/jid/valid/ipv6", "foo@2:0:1cfe:face:b00c::3", valid);
+  g_test_add_data_func ("/jid/valid/russian", "Анна@Каренина.ru", valid);
+
+  g_test_add_data_func ("/jid/invalid/empty", "", invalid);
+  g_test_add_data_func ("/jid/invalid/just-node", "foo@", invalid);
+  g_test_add_data_func ("/jid/invalid/node-and-resource", "foo@/lol", invalid);
+  g_test_add_data_func ("/jid/invalid/just-resource", "/lol", invalid);
+  g_test_add_data_func ("/jid/invalid/garbage", "(*&$(*)", invalid);
+  g_test_add_data_func ("/jid/invalid/space-domain", "foo bar", invalid);
+  g_test_add_data_func ("/jid/invalid/two-ats", "squid@cat@battle", invalid);
+
+#if 0
+  /* These are improperly accepted. */
+  g_test_add_data_func ("/jid/invalid/space-node", "i am a@fish", invalid);
+  /* U+00A0 NO-BREAK SPACE is in Table C.1.2, which is forbidden in domains. */
+  g_test_add_data_func ("/jid/invalid/nbsp-domain", "foo bar", invalid);
+#endif
+
+  result = g_test_run ();
+  test_deinit ();
+  return result;
+}
