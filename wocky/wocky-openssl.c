TelepathyIM/wocky

commit 9a3f3bc862a4ab368b231e96633326f4cafe4c6e

Not having a CRL for a cert that advertises one is not necessarily an error
Only stop if the user has requested strict checking. All it means is we
haven't downloaded the CRL and installed it on our system somewhere.

wocky/wocky-openssl.c
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2008 Christian Kellner, Samuel Cormier-Iijima
  * Copyright © 2008-2009 Codethink Limited
- * Copyright © 2009 Collabora Limited
+ * Copyright © 2009-2010 Collabora Limited
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -1071,6 +1071,11 @@ wocky_tls_session_verify_peer (WockyTLSSession    *session,
           break;
         case X509_V_ERR_PATH_LENGTH_EXCEEDED:
           *status = WOCKY_TLS_CERT_MAYBE_DOS;
+        case X509_V_ERR_UNABLE_TO_GET_CRL:
+          /* if we are in STRICT mode, being unable to see the CRL is a   *
+           * terminal condition: in NORMAL or LENIENT we can live with it */
+          if (level == WOCKY_TLS_VERIFY_STRICT)
+            *status = WOCKY_TLS_CERT_INSECURE;
           break;
         default:
           *status = WOCKY_TLS_CERT_UNKNOWN_ERROR;

commit 2cf2577a5465d8b13fa3d72e73cc384a68dd488d

OpenSSL: add a missing 'break'.
This was broken in 9a3f3bc8, which added the case for UNABLE_TO_GET_CRL.

Reviewed-by: Simon McVittie <simon.mcvittie@collabora.co.uk>

wocky/wocky-openssl.c
@@ -1071,6 +1071,7 @@ wocky_tls_session_verify_peer (WockyTLSSession    *session,
           break;
         case X509_V_ERR_PATH_LENGTH_EXCEEDED:
           *status = WOCKY_TLS_CERT_MAYBE_DOS;
+          break;
         case X509_V_ERR_UNABLE_TO_GET_CRL:
           /* if we are in STRICT mode, being unable to see the CRL is a   *
            * terminal condition: in NORMAL or LENIENT we can live with it */