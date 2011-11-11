16281498c3613e237b4096ee99789f80d02076c8
Alvaro Soliverez <alvaro.soliverez@collabora.co.uk>
2011-11-11 15:20:06 -0300

    Support building under Android, modified from an original patch by Derek Foreman
    https://bugs.freedesktop.org/show_bug.cgi?id=42509

---
+++ Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+WOCKY_BUILT_SOURCES := \
+	wocky/Android.mk
+
+wocky-configure-real:
+	cd $(WOCKY_TOP) ; \
+	CC="$(CONFIGURE_CC)" \
+	CFLAGS="$(CONFIGURE_CFLAGS)" \
+	LD=$(TARGET_LD) \
+	LDFLAGS="$(CONFIGURE_LDFLAGS)" \
+	CPP=$(CONFIGURE_CPP) \
+	CPPFLAGS="$(CONFIGURE_CPPFLAGS) -I$(GCRYPT_TOP)/src -I$(GPG_ERROR_TOP)/src" \
+	PKG_CONFIG_LIBDIR="$(CONFIGURE_PKG_CONFIG_LIBDIR)" \
+	PKG_CONFIG_TOP_BUILD_DIR=$(PKG_CONFIG_TOP_BUILD_DIR) \
+	$(WOCKY_TOP)/$(CONFIGURE) --host=arm-linux-androideabi \
+		--disable-Werror && \
+	for file in $(WOCKY_BUILT_SOURCES); do \
+		rm -f $$file && \
+		make -C $$(dirname $$file) $$(basename $$file) ; \
+	done
+
+wocky-configure: wocky-configure-real
+
+.PHONY: wocky-configure
+
+CONFIGURE_TARGETS += wocky-configure
+
+#include all the subdirs...
+-include $(WOCKY_TOP)/wocky/Android.mk
