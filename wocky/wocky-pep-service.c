f481ea91d5278178ca25abd9fd1f95283d2b0794
Will Thompson <will@willthompson.co.uk>
2012-01-30 17:08:23 +0000

    PepService: update © years.

--- wocky/wocky-pep-service.c
+++ wocky/wocky-pep-service.c
@@ -1,6 +1,6 @@
 /*
  * wocky-pep-service.c - WockyPepService
- * Copyright (C) 2009 Collabora Ltd.
+ * Copyright © 2009,2012 Collabora Ltd.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
