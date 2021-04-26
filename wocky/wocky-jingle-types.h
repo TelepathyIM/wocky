886e7a81032ff12d6f62101c985396d0e59699c7
Ruslan N. Marchenko <me@ruff.mobi>
2021-04-26 20:20:57 +0000

    DOC: fix/add documentation: wocky-jingle-types

--- wocky/wocky-jingle-types.h
+++ wocky/wocky-jingle-types.h
@@ -23,6 +23,7 @@
 #ifndef __WOCKY_JINGLE_TYPES_H__
 #define __WOCKY_JINGLE_TYPES_H__
 
+#ifndef __GTK_DOC_IGNORE__
 typedef struct _WockyJingleFactory WockyJingleFactory;
 typedef struct _WockyJingleSession WockyJingleSession;
 typedef struct _WockyJingleContent WockyJingleContent;
@@ -31,17 +32,25 @@ typedef struct _WockyJingleTransportRawUdp WockyJingleTransportRawUdp;
 typedef struct _WockyJingleTransportIceUdp WockyJingleTransportIceUdp;
 typedef struct _WockyJingleMediaRtp WockyJingleMediaRtp;
 typedef struct _WockyJingleCandidate WockyJingleCandidate;
+#endif /* __GTK_DOC_IGNORE__ */
 
-typedef enum { /*< skip >*/
-  /* not a jingle message */
+/**
+ * WockyJingleDialect:
+ * @WOCKY_JINGLE_DIALECT_ERROR: not a jingle message
+ * @WOCKY_JINGLE_DIALECT_GTALK3: relict libjingle3 gtalk variant
+ * @WOCKY_JINGLE_DIALECT_GTALK4: legacy and latest gtalk variant
+ * @WOCKY_JINGLE_DIALECT_V015: relict jingle version <ulink
+ *  url="https://xmpp.org/extensions/attic/xep-0166-0.15.html">0.15</ulink>
+ * @WOCKY_JINGLE_DIALECT_V032: old jingle version <ulink
+ *  url="https://xmpp.org/extensions/attic/xep-0166-0.32.html">0.32</ulink>
+ *
+ *  The type of jingle session dialect. Currently supports outdated dialects.
+ */
+typedef enum {
   WOCKY_JINGLE_DIALECT_ERROR,
-  /* old libjingle3 gtalk variant */
   WOCKY_JINGLE_DIALECT_GTALK3,
-  /* new gtalk variant */
   WOCKY_JINGLE_DIALECT_GTALK4,
-  /* jingle in the old 0.15 version days */
   WOCKY_JINGLE_DIALECT_V015,
-  /* current jingle standard */
   WOCKY_JINGLE_DIALECT_V032
 } WockyJingleDialect;
 
@@ -65,7 +74,7 @@ typedef enum { /*< skip >*/
  *
  * Possible states of a #WockyJingleSession.
  */
-typedef enum { /*< skip >*/
+typedef enum {
   /*< private >*/
   WOCKY_JINGLE_STATE_INVALID = -1,
   /*< public >*/
@@ -79,7 +88,57 @@ typedef enum { /*< skip >*/
   WOCKY_N_JINGLE_STATES
 } WockyJingleState;
 
-typedef enum { /*< skip >*/
+/**
+ * WockyJingleAction:
+ * @WOCKY_JINGLE_ACTION_UNKNOWN: Unknown/undefined action type
+ * @WOCKY_JINGLE_ACTION_CONTENT_ACCEPT: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-content-accept">
+ *  action="content-accept"</ulink>
+ * @WOCKY_JINGLE_ACTION_CONTENT_ADD: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-content-add">
+ *  action="content-add"</ulink>
+ * @WOCKY_JINGLE_ACTION_CONTENT_MODIFY: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-content-modify">
+ *  action="content-modify"</ulink>
+ * @WOCKY_JINGLE_ACTION_CONTENT_REMOVE: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-content-remove">
+ *  action="content-remove"</ulink>
+ * @WOCKY_JINGLE_ACTION_CONTENT_REPLACE: deprecated
+ * @WOCKY_JINGLE_ACTION_CONTENT_REJECT: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-content-reject">
+ *  action="content-reject"</ulink>
+ * @WOCKY_JINGLE_ACTION_SESSION_ACCEPT: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-session-accept">
+ *  action="session-accept"</ulink>
+ * @WOCKY_JINGLE_ACTION_SESSION_INFO: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-security-info">
+ *  action="security-info"</ulink>
+ * @WOCKY_JINGLE_ACTION_SESSION_INITIATE: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-session-initiate">
+ *  action="session-initiate"</ulink>
+ * @WOCKY_JINGLE_ACTION_SESSION_TERMINATE: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-session-terminate">
+ *  action="session-terminate"</ulink>
+ * @WOCKY_JINGLE_ACTION_TRANSPORT_INFO: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-transport-info">
+ *  action="transport-info"</ulink>
+ * @WOCKY_JINGLE_ACTION_TRANSPORT_ACCEPT: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-transport-accept">
+ *  action="transport-accept"</ulink>
+ * @WOCKY_JINGLE_ACTION_DESCRIPTION_INFO: <ulink
+ *  url="https://xmpp.org/extensions/xep-0166.html#def-action-description-info">
+ *  action="description-info"</ulink>
+ * @WOCKY_JINGLE_ACTION_INFO: deprecated
+ *
+ * The Jingle action as per <ulink
+ * url="http://xmpp.org/extensions/xep-0166.html#def-action">XEP-0166 Jingle
+ * §7.2</ulink> specification.
+ * Missing Actions (introduced in v0.35 with ns 'urn:xmpp:jingle:1'):
+ * * https://xmpp.org/extensions/xep-0166.html#def-action-security-info
+ * * https://xmpp.org/extensions/xep-0166.html#def-action-transport-replace
+ * * https://xmpp.org/extensions/xep-0166.html#def-action-transport-reject
+ */
+typedef enum {
   WOCKY_JINGLE_ACTION_UNKNOWN,
   WOCKY_JINGLE_ACTION_CONTENT_ACCEPT,
   WOCKY_JINGLE_ACTION_CONTENT_ADD,
@@ -116,7 +175,13 @@ typedef enum { /*< skip >*/
   WOCKY_JINGLE_TRANSPORT_PROTOCOL_TCP
 } WockyJingleTransportProtocol;
 
-typedef enum { /*< skip >*/
+/**
+ * WockyJingleCandidateType:
+ * @WOCKY_JINGLE_CANDIDATE_TYPE_LOCAL: local candidate (direct endpoint)
+ * @WOCKY_JINGLE_CANDIDATE_TYPE_STUN: candidate is mediated via STUN
+ * @WOCKY_JINGLE_CANDIDATE_TYPE_RELAY: candidate is mediated via TURN
+ */
+typedef enum {
   WOCKY_JINGLE_CANDIDATE_TYPE_LOCAL,
   WOCKY_JINGLE_CANDIDATE_TYPE_STUN,
   WOCKY_JINGLE_CANDIDATE_TYPE_RELAY
@@ -125,6 +190,23 @@ typedef enum { /*< skip >*/
 /**
  * WockyJingleReason:
  * @WOCKY_JINGLE_REASON_UNKNOWN: no known reason
+ * @WOCKY_JINGLE_REASON_ALTERNATIVE_SESSION: &lt;alternative-session/>
+ * @WOCKY_JINGLE_REASON_BUSY: &lt;busy/>
+ * @WOCKY_JINGLE_REASON_CANCEL: &lt;cancel/>
+ * @WOCKY_JINGLE_REASON_CONNECTIVITY_ERROR: &lt;connectivity-error/>
+ * @WOCKY_JINGLE_REASON_DECLINE: &lt;decline/>
+ * @WOCKY_JINGLE_REASON_EXPIRED: &lt;expired/>
+ * @WOCKY_JINGLE_REASON_FAILED_APPLICATION: &lt;failed-application/>
+ * @WOCKY_JINGLE_REASON_FAILED_TRANSPORT: &lt;failed-transport/>
+ * @WOCKY_JINGLE_REASON_GENERAL_ERROR: &lt;general-error/>
+ * @WOCKY_JINGLE_REASON_GONE: &lt;gone/>
+ * @WOCKY_JINGLE_REASON_INCOMPATIBLE_PARAMETERS: &lt;incompatible-parameters/>
+ * @WOCKY_JINGLE_REASON_MEDIA_ERROR: &lt;media-error/>
+ * @WOCKY_JINGLE_REASON_SECURITY_ERROR: &lt;security-error/>
+ * @WOCKY_JINGLE_REASON_SUCCESS: &lt;success/>
+ * @WOCKY_JINGLE_REASON_TIMEOUT: &lt;timeout/>
+ * @WOCKY_JINGLE_REASON_UNSUPPORTED_APPLICATIONS: &lt;unsupported-applications/>
+ * @WOCKY_JINGLE_REASON_UNSUPPORTED_TRANSPORTS:&lt;unsupported-transports/>
  *
  * The reason for a Jingle action occurring—specifically, the reason for
  * terminating a call. See <ulink
